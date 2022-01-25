f61922cd26b73cb969a435d301ffbbe8beb727fc
Lily <admin@mintlily.lgbt>
2022-01-25 01:34:06 -0500

    Change WebClient to HttpClient for .NET6

--- BSLegacyUtil_CLI/Utilities/UpdateCheck.cs
+++ BSLegacyUtil_CLI/Utilities/UpdateCheck.cs
@@ -1,24 +1,26 @@
 ﻿using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using static BSLegacyUtil.BuildInfo;
 
-namespace BSLegacyUtil.Utilities
-{
-    class UpdateCheck
-    {
+namespace BSLegacyUtil.Utilities {
+    class UpdateCheck {
         private static string lazyTag = "\"tag_name\": \"" + Version + "\"";
-        private static string incomingData = string.Empty;
         private static string GitHub = "https://api.github.com/repos/" + Author + "/BSLegacyUtil/releases";
+
+        private static async Task<string> GetString() {
+            var web = new HttpClient();
+            web.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0"); // Adds a "fake" client request
+            //web.DefaultRequestHeaders.Add("Content-Type", "application/json"); // Looks for JSON format
+            return await web.GetStringAsync(GitHub);
+        }
 
         public static void CheckForUpdates() {
             try {
-                WebClient web = new WebClient();
-                web.Headers["Content-Type"] = "application/json"; // Looks for JSON format
-                web.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0"); // Adds a "fake" client request
-                web.Encoding = Encoding.UTF8; // Get as plain text (No Special characters)
-                incomingData = web.DownloadString(GitHub);
+                var s = GetString().GetAwaiter().GetResult();
 
-                if (incomingData.Contains(lazyTag)) {
+                if (s.Contains(lazyTag)) {
                     if (Program.isDebug) Con.Log("Versions are matching");
                 } else Con.SendUpdateNotice();
             }
