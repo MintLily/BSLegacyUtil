ecbdcf7c7d3466c73b2ffae871312698d68ec2f9
Lily <admin@mintlily.lgbt>
2022-12-14 17:48:33 -0500

    Added remembered values

---
+++ BSLegacyUtil/Data/LocalJsonModel.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace BSLegacyUtil.Data;
+
+public class Config {
+    public string? RememberedVersion { get; set; }
+    public string? RememberedSteamUserName { get; set; }
+}
+
+public class LocalJsonModel {
+    public static Config? TheConfig { get; internal set; }
+    
+    public static void Start() {
+        if (!File.Exists($"{Vars.BaseDirectory}Config.json")) {
+            var c = new Config { RememberedVersion = "1.0.0", RememberedSteamUserName = ""};
+            File.WriteAllText($"{Vars.BaseDirectory}Config.json", JsonConvert.SerializeObject(c, Formatting.Indented));
+        }
+        
+        TheConfig = JsonConvert.DeserializeObject<Config>(File.ReadAllText($"{Vars.BaseDirectory}Config.json"));
+    }
+    
+    public static void Save() => File.WriteAllText($"{Vars.BaseDirectory}Config.json", JsonConvert.SerializeObject(TheConfig, Formatting.Indented));
+}
